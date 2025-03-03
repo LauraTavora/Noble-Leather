ac17ae94ac3f9b1b0b67a52111f9dcec8146ff5d
LauraTavora <146123654+LauraTavora@users.noreply.github.com>
2025-03-03 00:42:56 -0300

    Corrigindo pasta inacessível

---
+++ API/noble-leather-api/src/test/java/com/noble_leather/api_spring_app/ApiSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noble_leather.api_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
